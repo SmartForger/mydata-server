4facb644056bc0bc9a6aa4ec2ddc021e29cce934
SmartForger <23446619+SmartForger@users.noreply.github.com>
2018-12-23 10:11:24 -0800

    Init project

---
+++ index.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import cors from 'cors'
+import { ApolloServer, gql } from 'apollo-server-express'
+
+const app = express()
+
+app.use(cors())
+
+const schema = gql`
+  type Query {
+    me: User
+    user(id: ID!): User
+  }
+
+  type User {
+    id: ID!
+    username: String!
+  }
+`
+
+let users = {
+  1: {
+    id: '1',
+    username: 'Test User'
+  },
+  2: {
+    id: '2',
+    username: 'David Woods'
+  }
+}
+
+const me = users[1]
+
+const resolvers = {
+  Query: {
+    me: () => {
+      return me
+    },
+    user: (parent, { id }) => {
+      return users[id]
+    }
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on http://localhost:8000/graphql')
+})
