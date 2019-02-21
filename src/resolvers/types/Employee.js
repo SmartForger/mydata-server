d09c1101cff0febfaf57bf53f46eb1e0e3a078ff
SmartForger <23446619+SmartForger@users.noreply.github.com>
2019-02-21 02:21:18 -0800

    fix employee and stage

--- src/resolvers/types/Employee.js
+++ src/resolvers/types/Employee.js
@@ -1,4 +1,4 @@
-import { getCompany, getJob } from './utils'
+import { getCompany, getJob, getStage } from './utils'
 
 const getImageFromEmployee = async ({ id }, args, { models }) =>
   models.Images.findOne({
@@ -16,5 +16,6 @@ const getImageFromEmployee = async ({ id }, args, { models }) =>
 export default {
   Company: getCompany,
   Job: getJob,
+  Stage: getStage('stage_id'),
   Image: getImageFromEmployee
 }
